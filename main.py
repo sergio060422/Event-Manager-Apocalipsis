7412ac4266d26128bdb1e15c0cbc3688523a23c6
sergio22 <mr.sergiolooolll@gmail.com>
2025-10-23 17:16:03 -0400

    menu de seleccion de recursos

---
+++ main.py
@@ -0,0 +1,191 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '768')
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.widget import Canvas
+from kivy.uix.stacklayout import StackLayout
+from kivy.properties import ListProperty
+from kivy.core.window import Window
+from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty
+import json
+from kivy.lang import Builder
+from kivy.clock import Clock
+
+"""
+imagen del jugador
+"""
+class PlayerLayout(BoxLayout):
+    def __init__(self):
+        super().__init__()
+"""
+clase de herramientas para usos variados:
+get_all: retorna todos los recursos
+get_one: retorna el recurso asignado a un id
+appList: retorna la lista de componentes existentes en la App
+"""
+class Utils():
+    def get_all():
+        with open("recursos.json") as file:
+            return json.load(file)
+    def get_one(id):
+         with open("recursos.json") as file:
+            return json.load(file)[id - 1]
+    def appList():
+        return App.get_running_app()
+    rList = []
+"""
+clase asignado a cada recurso especifico
+contiene una foto con la imagen del recurso
+"""
+class Resource(FloatLayout):
+    def __init__(self, item):
+        super().__init__()
+        self.icon = Image(source=f"assets/{item}.png")
+        self.icon.size_hint = (None, None)
+        self.icon.size = (60, 60)
+        self.icon.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        self.add_widget(self.icon)
+        self.selected = 0
+        self.id = item
+
+    my_color = ListProperty([0.1, 0.1, 0.1, 1])
+    """
+    funcion para la seleccion del evento 
+    """
+    def add_resource(self):
+        recurso = Utils.get_one(self.id)
+        with open("recursos_seleccionados.json", "w") as file:
+            json.dump(recurso, file, indent=4)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            menu = Utils.appList().menu
+            resource = Utils.get_one(self.id)
+            """
+            condicional para comprobar si el evento esta seleccionado o no
+                esta seleccionado:
+                    -lo deselecciona cambiando el color del borde y si es el unico evento seleccionado cambia la transparencia(opacity) del menu de evento a 0 (lo hace invisible)
+                    -comprueba si el evento que se esta mostrando en el menu de eventos es el mismo que el evento que deseleccione para cambiar la visibilidad de este o no
+                si no esta seleccionado:
+                    -lo selecciona cambiando el color del borde 
+                    -comprueba si el menu de eventos donde debe ir la informacion del evento seleccionado esta activo(es decir si es el primer evento seleccionado)
+            """
+            if not self.selected:
+                self.my_color = [0, 0.8, 0.6, 0.8]
+                self.selected = 1
+                """
+                condicional para comprobar si el menu de eventos no esta activo:
+                    -actualiza la informacion con la del evento pulsado 
+                    -cambia la transparencia(opacity) a 1 (lo muestra)
+                """
+                if not EventMenu.active:
+                    info = menu.rInfo
+                    info.img_source = f"assets/{self.id}.png"
+                    info.name = resource["nombre"]
+                    info.description = resource["descripcion"]
+                    info.complementary = resource["complementario"][0]
+
+                    infotype = ""
+                   
+                    for i in range(len(resource["tipo"])):
+                        infotype += resource["tipo"][i]
+
+                        if i < len(resource["tipo"]) - 1:
+                            infotype += ", "
+
+                    info.type = infotype
+                    
+                    comp = ""
+
+                    for i in range(len(resource["complementario"])):
+                        comp += resource["complementario"][i]
+
+                        if i < len(resource["complementario"]) - 1:
+                            comp += ", "
+
+                    info.complementary = comp
+
+                    info.opacity = 1
+                    self.add_resource()              
+            else:
+                self.my_color = [0.1, 0.1, 0.1, 1]
+                self.selected = 0
+                
+                if menu.rInfo.name == resource["nombre"]:
+                    menu.rInfo.opacity = 0
+                
+"""
+componente donde van los recursos
+"""
+class ResourceList(StackLayout):
+    def __init__(self):
+        super().__init__()
+        self.orientation = 'lr-tb'
+        for i in range(1, 25):
+            self.add_widget(Resource(i))
+    selected = []
+
+"""
+componente con la informacion que debe ir dentro del menu de eventos
+"""
+class ResourceInfoLayout(StackLayout):
+    def __init__(self):
+        super().__init__()
+    
+    screen = BooleanProperty(False)
+    
+    img_source = StringProperty("assets/")
+    name = StringProperty("")
+    type = StringProperty("")
+    description = StringProperty("")
+    complementary = StringProperty("")
+   
+"""
+componente contenedor del inventario completo de recursos
+"""    
+class ResourcesLayout(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.add_widget(ResourceList(), index=0)
+        self.add_widget(Label(), index=0)
+        
+"""
+contenedor del jugador con su inventario
+"""
+class ConfigEvent(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        self.size_hint = (None, None)
+        self.size = (700, 500)
+        self.orientation = "horizontal"
+        self.add_widget(PlayerLayout())
+        self.add_widget(ResourcesLayout())
+"""
+componente que muestra la informacion del evento seleccionado, se situa en la esquina superior izquierda
+"""
+class EventMenu(FloatLayout):
+    def __init__(self):
+        super().__init__()
+        self.background = Image(source="assets/background_main.png")
+        self.add_widget(self.background)
+        self.add_widget(ConfigEvent())
+        self.rInfo = ResourceInfoLayout()
+        self.add_widget(self.rInfo)
+    active = 0
+"""
+cuerpo de la aplicacion
+"""
+class Main(App):
+    def build(self):        
+        self.menu = EventMenu()
+        return self.menu
+        
+Main().run()
---
+++ recursos.md
@@ -0,0 +1,98 @@
+Los recursos tendran tres tipos: supervivencia, defensa, y refugio, cada recurso ira seguido de su complementario
+
+1) Escopeta de caza
+-Descripcion: Escopeta de caza calibre 50, ten cuidado apretando el gatillo o podrias acabar decorando el techo del refugio!
+-Tipo: Defensa
+-Complementario: Balas de escopeta
+2) Balas de escopeta
+-Descripcion: Balas de escopeta, puede que los magos las detengan con la boca pero los caminantes no son magos
+-Tipo: Defensa
+-Complementario: Escopeta de caza
+3) Baby Shark
+-Descripcion: El mote de este revolver Magnum calibre 44 fue puesto por el abuelo, quien afirmaba que se podian cazar tiburones con el!
+-Tipo: Defensa 
+-Complementario: Balas del Baby Shark
+4) Balas del Baby Shark
+-Descripcion: Balas para revolver calibre 44, pueden ser usadas con el Baby Shark, tienen tallado un tiburon con zapatos!
+-Tipo: Defensa
+-Complementario: Baby Shark
+5) Osvaldo el mulato
+-Descripcion: Este emigrante era un antiguo carnicero en su tierra natal Cuba, dicen que es capaz de cortar cualquier cosa con sus machetes, aunque nadie que piense lo contrario ha vivido para contarlo
+-Tipo: Defensa
+-Complementario: Machetes oxidados 
+6) Machetes oxidados
+-Descripcion: Solo Osvaldo sabe como usar estos machetes oxidados, no le gusta que los toquen
+-Tipo: Defensa
+-Complementario: Osvaldo el mulato y Piedra de afilar
+7) Rocky 
+-Descripcion: Este cachorro de Husky Siberiano fue el unico superviviviente junto al abuelo del ataque que sufrio el actual refugio antes de nuestra llegada, le gustan mucho las galletas
+-Tipo: Defensa, Refugio
+-Complementario: Galletas en forma de hueso
+8) Galletas en forma de hueso   
+-Descripcion: Estas galletas son las favoritas de Rocky, lanzalas encima de los caminantes y deja que haga el trabajo!
+-Tipo: Defensa, Refugio
+-Complementario: Rocky
+9) Milly, la mecaninca
+-Descripcion: El primer juguete que Milly tuvo entre sus mmanos durante la infancia fue una llave inglesa, siempre fue muy unida a su padre y desde que este fallecio en los sucesos de la primera oleada no ha sido la misma, ahora lucha junto a tu grupo para encontrar la cura al virus
+-Tipo: Refugio
+-Complementario: Llave inglesa de papa
+10) LLave inglesa de papa
+-Descripcion: El primer juguete que tuvo Milly, se la dio su padre, puede reparar cualquier cosa con ella
+-Tipo: Refugio
+-Complementario: Milly, la mecanica
+11) Kit de Mantenimiento de Refugio(KMR)
+-Descripcion: Este kit creado por Milly permite dar mantenimiento a las maquinarias del refugio sin tener grandes conocimientos de mecanica, no contiene instrucciones
+-Tipo: Refugio
+-Complementario: Aceite de Motor
+12) Aceite de Motor
+-Descripcion: Aceite de motor comun y corriente, ideal para maquinarias chirriantes
+-Tipo: Refugio 
+-Complmentario: Kit de Mantenimiento de Refugio(KMR)
+13) Kit de Seguridad de Refugio(KSR)
+-Descripcion: Este kit creado por el abuelo es lo mejor para realizar reparaciones en las areas mas sensibles del refugio, despues de la ultima oleada fue perfeccionado y actualizado
+-Tipo: Refugio
+-Complementario: Placa Reforzante 
+14) Placa Reforzante
+-Descripcion: Como su nombre lo indica esta placa fabricada con fibra de carbono puede reforzar cualquier tipo de estructura del refugio con tan solo un par de martillazos, se instala con el KSR
+-Tipo: Refugio
+-Complementario: Kit de Mantenimiento de Refugio(KSR)
+15) Polkish, el abuelo
+-Descripcion: Polkish (el abuelo) era antiguamente un experimentado explorador que viajaba con su familia por todo el mundo realizando proezas de supervivencia. Cuando comenzo la epidemia se mudo al refugio y gracias a sus conocimientos amplio la seguridad haciendolo inexpugnable durante largo tiempo hasta la llegada de la ultima ola, la cual arraso con todos los que vivian ahi incluyendo a su familia, solo el y su perro Rocky sobrevivieron a la catastrofe, ahora forma parte de tu grupo que intenta reconstruir lo que en su tiempo fue el mayor bastion de la humanidad contra los caminantes
+-Tipo: Refugio, Supervivencia
+-Complementario: Ziggy el filoso
+16) Ziggy el filoso
+-Descripcion: Este cuchillo Bowie pertenece al abuelo, tiene un filo descomunal, parece que fue traido de otro planeta!
+-Tipo: Supervivencia
+-Complementario: Polkish, el abuelo y Piedra de afilar
+17) Pathfinder
+-Descripcion: El Pathfinder es un Voksgawen modificado del ano 68, sirve para realizar exploraciones rapidas al exterior
+-Tipo: Supervivencia
+-Complementario: Galon de gasolina
+18) Galon de gasolina
+-Descripcion: Un galon de gasonlina, huele rico
+-Tipo: Supervivencia
+-Complementario: Pathfinder o TIBURON
+19) TIBURON
+-Descripcion: Esta moto partenecio al abuelo en sus a;os de juventud cuando formaba parte de una banda de motoristas exploradores, esta equipado con una escopeta cartucho adaptada
+-Tipo: Supervivencia, Defensa
+-Complementario: Galon de gasolina y Escopeta recortada adaptada
+20) Escopeta recortada adaptada
+-Descripcion: Escopeta recortada italiana de 2 proyectiles, el abuelo la llama: Abremundos
+-Tipo: Superivencia, Defensa
+-Complementario: Balas de escopeta
+21) Raciones enlatadas
+-Descripcion: Comida enlatada ideal para todo tipo de exploracion, no tiene mucho sabor
+-Tipo: Supervivencia
+-Complementario: Abrelatas
+22) Abrelatas
+-Descripcion: Abrelatas marca OpenAll, sirve para abrir todo tipo de latas
+-Tipo: Supervivencia
+-Complementario: Raciones enlatadas
+23) Navaja Suiza de Osvaldo
+-Descripcion: Navaja Suiza hecha en Cuba, sirve para todo!
+-Tipo: Supervivencia, Refuegio, Defensa
+-Complementario: Piedra de afilar
+24) Piedra de afilar
+-Descripcion: Piedra de afilar para filos menores, aunque Osvaldo la usa con sus machetes
+-Tipo: Supervivencia, Refugio, Defensa
+-Complementario: Navaja Suiza de Osvaldo, Machetes oxidados
